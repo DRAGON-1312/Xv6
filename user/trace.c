a23cdd6e111fbc14f3fd93d7da0aabbeb41e04b9
hbgia <hbgia2404@clc.fitus.edu.vn>
2026-03-11 17:41:02 +0700

    [FIXED] build failed because I forgot to add trace to makefile and forgot to write trace.c

---
+++ user/trace.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 3){
+        fprintf(2, "Usage: %s mask command\n", argv[0]);
+        exit(1);
+    }
+
+    if(trace(atoi(argv[1])) < 0){
+        fprintf(2, "%s: trace failed\n", argv[0]);
+        exit(1);
+    }
+
+    exec(argv[2], argv + 2);
+    exit(0);
+}
